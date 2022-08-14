aa153793516c3442ac74215c24e87a81bb7c9ac8
יעקב גיגי <yaccovgigi@hotmail.com>
2022-08-14 19:22:30 +0300

    add gsheet helper

---
+++ Hashtil Jobs For Drivers/Heplers/GSheetsHelper.cs
@@ -0,0 +1,11 @@
+﻿using Google.Apis.Sheets.v4;
+
+namespace Hashtil_Jobs_For_Drivers.Heplers
+{
+    public class GSheetsHelper
+    {
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static readonly string ApplicationName = "JobsForDrivers";
+        static readonly string SpreadsheetId = "";
+    }
+}
