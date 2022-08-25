a0f8b60f1c1dc44aa6cad7ec459ddf5f693a87c8
יעקב גיגי <yaccovgigi@hotmail.com>
2022-08-25 15:11:23 +0300

    add green house capacity

--- Hashtil Jobs For Drivers/Models/GreenHouse.cs
+++ Hashtil Jobs For Drivers/Models/GreenHouse.cs
@@ -1,10 +1,12 @@
-﻿namespace Hashtil_Jobs_For_Drivers.Models
+﻿using System;
+
+namespace Hashtil_Jobs_For_Drivers.Models
 {
     public class GreenHouse
     {
         public int GreenNum { get; set; }
         public int CurrentMagash { get; set; }
         public int MaxMagash { get; set; }
-        public string PrecentOfOcuupancy => (CurrentMagash/MaxMagash).ToString("0.00%");
+        public int PrecentOfOcuupancy => Convert.ToInt32((Convert.ToDouble(CurrentMagash)/Convert.ToDouble(MaxMagash)*100));
     }
 }
--- Hashtil Jobs For Drivers/UserControlScreens/DashBoardControl.xaml.cs
+++ Hashtil Jobs For Drivers/UserControlScreens/DashBoardControl.xaml.cs
@@ -24,6 +24,8 @@ namespace Hashtil_Jobs_For_Drivers.UserControlScreens
     public partial class DashBoardControl : UserControl
     {
         DashBoardData DashboardData = new DashBoardData();
+        List<DashBoardDataPhp> DashboardDataPhp = new List<DashBoardDataPhp>();
+        List<GreenHouse> GreenHouseExcelList = new List<GreenHouse>();
         public DashBoardControl()
         {
             InitializeComponent();
@@ -42,18 +44,27 @@ namespace Hashtil_Jobs_For_Drivers.UserControlScreens
 
         private async void GetAllDataAsync()
         {
-            var phpGreedList = await ApiHelper.GetGreenHouseDataMySql();
-            var greenHouseList = await ExcelHelper.GetListOfGreenHouse();
-            DashboardData = await Heplers.GSheetsHelper.DashBoardData();
+            DashboardDataPhp = await ApiHelper.GetGreenHouseDataMySql();
+            GreenHouseExcelList = await ExcelHelper.GetListOfGreenHouse();
+            //DashboardData = await Heplers.GSheetsHelper.DashBoardData();
             await Dispatcher.BeginInvoke(new ThreadStart(() => LSpinner.Visibility = Visibility.Hidden));
             await this.Dispatcher.BeginInvoke(new ThreadStart(() =>
             {
-                // Jobs fot T
-                CPBJobsForT.Progress = Convert.ToDouble(DashboardData.NumOfOrdersForTommorrow);
-                // Treys fot T
-                CPBTreysForT.Progress = Convert.ToDouble(DashboardData.NumOfMagashForTommorrow);
-                // Plants fot T
-                CPBplantsForT.Progress = Convert.ToDouble(DashboardData.NumOfPlantsForTommorrow);
+                // G1 Occupancy Treys
+                CPBG1.Progress = Convert.ToDouble(GreenHouseExcelList.FirstOrDefault(x => x.GreenNum == 1).PrecentOfOcuupancy);
+                // G2 Occupancy Treys
+                CPBG2.Progress = Convert.ToDouble(GreenHouseExcelList.FirstOrDefault(x => x.GreenNum == 2).PrecentOfOcuupancy);
+                // G3 Occupancy Treys
+                CPBG3.Progress = Convert.ToDouble(GreenHouseExcelList.FirstOrDefault(x => x.GreenNum == 3).PrecentOfOcuupancy);
+                // G4 Occupancy Treys
+                CPBG4.Progress = Convert.ToDouble(GreenHouseExcelList.FirstOrDefault(x => x.GreenNum == 4).PrecentOfOcuupancy);
+                // G5 Occupancy Treys
+                CPBG1.Progress = Convert.ToDouble(GreenHouseExcelList.FirstOrDefault(x => x.GreenNum == 5).PrecentOfOcuupancy);
+                ///// G6 Occupancy Treys
+                CPBG5.Progress = Convert.ToDouble(GreenHouseExcelList.FirstOrDefault(x => x.GreenNum == 6).PrecentOfOcuupancy);
+                // G7 Occupancy Treys
+                CPBG7.Progress = Convert.ToDouble(GreenHouseExcelList.FirstOrDefault(x => x.GreenNum == 7).PrecentOfOcuupancy);
+               
             }));
 
         }
