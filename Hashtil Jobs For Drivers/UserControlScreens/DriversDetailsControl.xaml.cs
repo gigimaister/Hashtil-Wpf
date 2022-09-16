48572befea347d5762e7b5ae09bb20711c8c6aca
יעקב גיגי <yaccovgigi@hotmail.com>
2022-09-16 10:56:59 +0300

    add drivers control + thai info control

--- Hashtil Jobs For Drivers/UserControlScreens/DriversDetailsControl.xaml.cs
+++ Hashtil Jobs For Drivers/UserControlScreens/DriversDetailsControl.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Hashtil_Jobs_For_Drivers.Heplers;
+using Hashtil_Jobs_For_Drivers.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Hashtil_Jobs_For_Drivers.UserControlScreens
 {
@@ -20,9 +25,24 @@ namespace Hashtil_Jobs_For_Drivers.UserControlScreens
     /// </summary>
     public partial class DriversDetailsControl : UserControl
     {
+        List<Driver> Drivers = new List<Driver>();
         public DriversDetailsControl()
         {
             InitializeComponent();
+            Task.Run(() => GetDrivers());
         }
+
+        private async  void GetDrivers()
+        {
+            Drivers = await ApiHelper.GetDrivers();
+
+            await this.Dispatcher.BeginInvoke(new ThreadStart(() =>
+            {
+                icDrivers.ItemsSource = Drivers;
+                LSpinner.Visibility = Visibility.Collapsed;
+            }));
+        }
+
+     
     }
 }
